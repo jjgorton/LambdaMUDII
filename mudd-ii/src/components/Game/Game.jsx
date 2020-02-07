32d87db3486abc4be68370a60d9d1862df4d30e0
jjgorton <jjgorton@gmail.com>
2020-02-06 23:22:37 -0500

    removed explicit async stuff - manual play works!

--- mudd-ii/src/components/Game/Game.jsx
+++ mudd-ii/src/components/Game/Game.jsx
@@ -17,10 +17,14 @@ const player = new Player();
 
 const Game = props => {
     const [curRoomId, setCurRoomId] = useState();
+    const [prevRoom, setPrevRoom] = useState();
     // const [roomID, setRoomID] = useState();
     const [coolDown, setCoolDown] = useState(0);
     const [counter, setCounter] = useState(coolDown);
     const [flip, setFlip] = useState(false);
+    const [travel, setTravel] = useState(false);
+    const [direct, setDirect] = useState({});
+    const [newRoom, setNewRoom] = useState({});
 
     useEffect(() => {
         axios
@@ -30,6 +34,29 @@ const Game = props => {
                 graph.rooms = res.data;
             })
             .catch(err => console.error(err));
+    }, []);
+
+    useEffect(() => {
+        if (counter === 0) {
+            axiosWithAuth()
+                .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/')
+                .then(res => {
+                    console.log('RES.DATA(init):', res.data);
+                    graph.add_room(res.data);
+                    // setNewRoom(res.data);
+                    setCoolDown(res.data.cooldown);
+                    setFlip(!flip);
+                    setCurRoomId(res.data.room_id);
+                    player.update_room(res.data.room_id);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+            // });
+        } else {
+            // alert(`Cool Down has ${counter} more seconds to go`);
+            console.error(`Cool Down has ${counter} more seconds to go`);
+        }
     }, []);
 
     useEffect(() => {
@@ -47,192 +74,117 @@ const Game = props => {
         }, 1000);
     }, [flip]);
 
-    // useEffect(() => {
-    //     setRoomID(curRoomId);
-    //     console.log('inside EFECT', curRoomId);
-    // }, [curRoomId]);
-
-    // const timer = seconds => {
-    //     console.log('timer called!!!');
-    //     const waitA = setInterval(() => {
-    //         let time = seconds;
-    //         if (time > 0) {
-    //             time -= 1;
-    //             setCounter(time);
-    //             console.log('cd', time);
-    //         }
-    //         if (time <= 0) {
-    //             setCounter(0);
-    //             // return true;
-    //             clearInterval(waitA);
-    //         }
-    //     }, 1000);
-    // };
-
-    const wait = seconds => {
-        let makeMS = seconds * 1000;
-        return new Promise((res, rej) => setTimeout(res, makeMS));
-    };
-
-    const move = direction => {
-        //check if valid direction
-        return new Promise((res, rej) => {
-            if (!graph.rooms[player.get_room_id()].exits[direction]) {
-                alert(`Cannot move ${direction}!`);
-            } else {
-                //setup timer for cooldown
-                // wait(coolDown).then(() => {
-                if (counter === 0) {
-                    //Wise Explorer Bonus:
-                    const nextRoomId =
-                        graph.rooms[player.get_room_id()].exits[direction];
-                    const moveObj =
-                        nextRoomId >= 0
-                            ? {
-                                  direction: direction,
-                                  next_room_id: `${nextRoomId}`
-                              }
-                            : { direction: direction };
-                    console.log('request obj:', moveObj);
-                    axiosWithAuth()
-                        .post(
-                            'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/',
-                            moveObj
-                        )
-                        .then(res => {
-                            console.log('RES.DATA(move):', res.data);
-                            if (res.data.errors.length > 0) {
-                                alert(res.data.errors);
-                            } else {
-                                console.log('MOVE - ELSE', graph);
-                                graph.add_room(res.data);
-                                console.log('109', graph);
-                                graph.add_connection(
-                                    player.prev_room_id,
-                                    res.data.room_id,
-                                    direction
-                                );
-                                console.log('115', graph);
-                                setCoolDown(res.data.cooldown);
-                                setFlip(!flip);
-                                console.log('COOLDOWN', res.data.cooldown);
-                                // setStartTime(Date.now());
-                                // timer(res.data.coolDown);
-                                setCurRoomId(res.data.room_id);
-                                console.log('playerRoom', player.get_room_id());
-                                player.update_room(res.data.room_id);
-                                console.log(
-                                    'after playerRoom',
-                                    player.get_room_id()
-                                );
-
-                                axios
-                                    .post(
-                                        'http://localhost:5000/rooms',
-                                        graph.rooms
-                                    )
-                                    .then(res => console.log('saved'))
-                                    .catch(err => console.error(err));
-                            }
-                        })
-                        .catch(err => {
-                            console.log(err.message);
-                        });
-                    // });
+    function MoveAPI(direct) {
+        axiosWithAuth()
+            .post(
+                'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/',
+                direct
+            )
+            .then(res => {
+                console.log('RES.DATA(move):', res.data);
+                if (res.data.errors.length > 0) {
+                    alert(res.data.errors);
                 } else {
-                    // alert(`Cool Down has ${counter} more seconds to go`);
-                    console.error(
-                        `Cool Down has ${counter} more seconds to go`
-                    );
+                    setPrevRoom(player.get_room_id());
+                    setNewRoom(res.data);
                 }
+            })
+            .catch(err => console.error(err));
+    }
+
+    //check if valid direction
+    function validMove(direction) {
+        console.log(
+            `room: ${player.get_room_id()}\n
+            exits: ${Object.keys(graph.rooms[player.get_room_id()].exits)}\n
+            direction-key: ${direction}\n
+            direction-value: ${
+                graph.rooms[player.get_room_id()].exits[direction]
             }
-        });
-    };
+        `
+        );
+        let doors = Object.keys(graph.rooms[player.get_room_id()].exits);
+        if (!doors.includes(direction)) {
+            alert(`Cannot move ${direction}!`);
+            return false;
+        } else {
+            setDirect(direction);
+            //Wise Explorer Bonus:
+            const nextRoomId =
+                graph.rooms[player.get_room_id()].exits[direction];
+            const moveObj =
+                nextRoomId >= 0
+                    ? {
+                          direction: direction,
+                          next_room_id: `${nextRoomId}`
+                      }
+                    : { direction: direction };
+            console.log('request obj:', moveObj);
+            return moveObj;
+        }
+    }
 
     useEffect(() => {
-        // wait(coolDown).then(() => {
+        if (newRoom.title) {
+            console.log(newRoom);
+            graph.add_room(newRoom);
+            console.log('prev:', player.prev_room_id, 'cur:', newRoom.room_id);
+            graph.add_connection(
+                player.current_room_id,
+                newRoom.room_id,
+                direct
+            );
+            console.log('124', graph);
+            setCoolDown(newRoom.cooldown);
+            setFlip(!flip);
+            console.log('COOLDOWN', newRoom.cooldown);
+            setCurRoomId(newRoom.room_id);
+            console.log('playerRoom', player.get_room_id());
+            player.update_room(newRoom.room_id);
+            console.log('after playerRoom', player.get_room_id());
+        }
+    }, [newRoom]);
+
+    const move = direction => {
+        //setup timer for cooldown
         if (counter === 0) {
-            axiosWithAuth()
-                .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/')
-                .then(res => {
-                    console.log('RES.DATA(init):', res.data);
-                    graph.add_room(res.data);
-                    setCoolDown(res.data.cooldown);
-                    setFlip(!flip);
-                    // setStartTime(Date.now());
-                    // timer(res.data.coolDown);
-                    setCurRoomId(res.data.room_id);
-                    player.update_room(res.data.room_id);
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-            // });
+            const directObj = validMove(direction);
+            if (directObj) MoveAPI(directObj);
+            // graph.add_room(newRoom);
+            // graph.add_connection(
+            //     player.current_room_id,
+            //     newRoom.room_id,
+            //     direction
+            // );
+            // console.log('124', graph);
+            // setCoolDown(newRoom.cooldown);
+            // setFlip(!flip);
+            // console.log('COOLDOWN', newRoom.cooldown);
+            // setCurRoomId(newRoom.room_id);
+            // console.log('playerRoom', player.get_room_id());
+            // player.update_room(newRoom.room_id);
+            // console.log('after playerRoom', player.get_room_id());
+            axios
+                .post('http://localhost:5000/rooms', graph.rooms)
+                .then(res => console.log('saved'))
+                .catch(err => console.error(err));
         } else {
             // alert(`Cool Down has ${counter} more seconds to go`);
             console.error(`Cool Down has ${counter} more seconds to go`);
         }
-    }, []);
+    };
+
     console.log(graph);
-    // setInterval(() => console.log(coolDown), 2000);
-
-    // const automate = async () => {
-    //     const traversal_path = [];
-    //     const unexplored = [];
-    //     unexplored.push(curRoomId);
-    //     const visited = {};
-    //     const path = [];
-
-    //     while (unexplored.length > 0) {
-    //         let room = unexplored[unexplored.length - 1];
-    //         visited[room] = 'added';
-    //         console.log(visited);
-
-    //         let direction = graph.check_for_unexplored(room);
-    //         console.log('Before move', curRoomId);
-    //         if (direction && room === curRoomId) {
-    //             console.log('IF passed!', coolDown);
-    //             await wait(coolDown);
-    //             console.log('After wait()');
-    //             await move(direction[0]);
-    //             traversal_path.push(direction[0]);
-    //             path.push(direction[0]);
-    //             console.log('AFTER move', curRoomId);
-    //             visited[curRoomId] = 'added';
-    //         } else {
-    //             console.log('ELSE:', direction, room, curRoomId);
-    //             while (!graph.check_for_unexplored(curRoomId)) {
-    //                 if (path.length > 0) {
-    //                     let backtrack = graph.reverse(path.pop());
-    //                     await wait(coolDown);
-    //                     await move(backtrack);
-    //                     traversal_path.push(backtrack);
-    //                 } else {
-    //                     console.log('BREAK');
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //         if (!graph.check_for_unexplored(room)) {
-    //             unexplored.pop();
-    //         }
-    //         if (graph.check_for_unexplored(curRoomId)) {
-    //             if (!unexplored.includes(curRoomId)) {
-    //                 unexplored.push(curRoomId);
-    //             }
-    //         }
-    //     }
-
-    //     return Object.keys(visited).length;
-    // };
 
     return (
         <>
             <NavBarLogout {...props} />
-            {/* <h1>Game</h1> */}
-            {/* <Player {...props} /> */}
             <div className='display'>
-                <Display rooms={graph.rooms} curRoomId={curRoomId} />
+                <Display
+                    rooms={graph.rooms}
+                    curRoomId={curRoomId}
+                    player={player}
+                />
             </div>
             <Controls
                 move={move}
@@ -240,11 +192,13 @@ const Game = props => {
                 rooms={graph.rooms}
                 curRoomId={curRoomId}
                 counter={counter}
+                player={player}
             />
 
             <Auto
                 move={move}
                 coolDown={coolDown}
+                counter={counter}
                 graph={graph}
                 player={player}
                 curRoomId={curRoomId}
--- mudd-ii/src/utils/player.js
+++ mudd-ii/src/utils/player.js
@@ -1,12 +1,17 @@
 class Player {
     constructor() {
         this.current_room_id = 0;
-        this.prev_room_id = null;
+        this.prev_room_id = this.current_room_id;
     }
 
     update_room(room_id) {
         this.prev_room_id = this.current_room_id;
         this.current_room_id = room_id;
+        console.log(
+            'UPDATED PLAYERS ROOM',
+            this.current_room_id,
+            this.prev_room_id
+        );
     }
 
     get_room_id() {
